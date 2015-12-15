c24763e62b889fddeb0d0e8971a6593c9bd0570d
jim leach <yogajim@gmail.com>
2015-12-15 09:03:16 -0500

    initial

---
+++ Readme.md
@@ -0,0 +1,5 @@
+## Welcome to My Repo
+
+> My personal website
+
+
---
+++ js/main.js
@@ -0,0 +1,12 @@
+// console.log('This is cool!');
+
+// $('.container').html(template.example({
+//   value: 'Hello, World!!'
+// }));
+
+
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
+
